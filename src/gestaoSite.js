7ecc969bf8d200629aab0e3e419e7f6a44f623f2
danielColona <danielcolona@gmail.com>
2026-05-28 20:16:15 -0300

    feat: add user authentication functionality with tests and report generation
    - Implemented user authentication logic in src/gestaoSite.js
    - Added unit tests for authentication scenarios in test/gestaoSite.test.js
    - Integrated Mochawesome for test reporting with generated HTML and JSON reports
    - Included Roboto font files for report styling
    - Updated package.json to include mochawesome as a dev dependency

---
+++ src/gestaoSite.js
@@ -0,0 +1,48 @@
+const usuarios = [
+    { id: 1,
+      nome: 'Daniel Colona', 
+      email: 'danielcolona@gmail.com',
+      senha: '12345678',
+      expirado: false
+    },
+    { id: 2,
+      nome: 'Maria Silva', 
+      email: 'mariasilva@gmail.com',
+      senha: '87654321',
+      expirado: false
+    },
+    { id: 3,
+      nome: 'João Pereira', 
+      email: 'joaopereira@gmail.com',
+      senha: '11223344',
+      expirado: false
+    },
+    { id: 4,
+      nome: 'Ana Souza', 
+      email: 'anasouza@gmail.com',
+      senha: '44332211',
+      expirado: true
+    }
+]
+
+console.log(usuarios)
+
+export function autenticarUsuario(email, senha) {
+
+    for(let i = 0; i < usuarios.length; i++) {
+        if(usuarios[i].email == email) {
+            if(usuarios[i].senha == senha) {
+                if(usuarios[i].expirado) {
+                    return 'Renove suas credenciais'
+                } else {
+                    return 'Login realizado com sucesso'
+                }
+            } else {    
+                return 'Senha incorreta para o email informado'
+            }
+        }
+    }
+    return 'Usuário não encontrado'
+}
+
+console.log(autenticarUsuario('joaopereira@gmail.com', '11223344'))
---
+++ test/gestaoSite.test.js
@@ -0,0 +1,48 @@
+//Escreva 4 testes: 1) Sucesso, 2) Credencial expirada,
+//  3) Usuario não encontrado e 4) Senha incorreta para o usuário encontrado.
+
+import { autenticarUsuario } from '../src/gestaoSite.js'
+import assert from 'node:assert'
+
+describe('Teste de autenticação de usuário', function() {
+
+    it('Deve autenticar o usuário com sucesso', function() {
+        //Arrange
+        const email = 'danielcolona@gmail.com'
+        const senha = '12345678'
+        //Act        
+        const resultado = autenticarUsuario(email, senha);
+        //Assert    
+        assert.equal(resultado, 'Login realizado com sucesso');
+    })
+
+    it('Deve retornar mensagem de credencial expirada', function() {
+        //Arrange
+        const email = 'anasouza@gmail.com'
+        const senha = '44332211'
+        //Act
+        const resultado = autenticarUsuario(email, senha);
+        //Assert
+        assert.equal(resultado, 'Renove suas credenciais');
+    })  
+
+    it('Deve retornar mensagem de usuário não encontrado', function() {
+        //Arrange
+        const email = 'usuariodesconhecido@gmail.com'
+        const senha = '12345678'
+        //Act
+        const resultado = autenticarUsuario(email, senha);
+        //Assert
+        assert.equal(resultado, 'Usuário não encontrado');
+    })
+
+    it('Deve retornar mensagem de senha incorreta', function() {
+        //Arrange
+        const email = 'danielcolona@gmail.com'
+        const senha = '87654321'
+        //Act
+        const resultado = autenticarUsuario(email, senha);
+        //Assert
+        assert.equal(resultado, 'Senha incorreta para o email informado');
+    })  
+})
